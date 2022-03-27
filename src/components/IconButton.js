082ba9ac77742c895e681f83ae24a8faaff9e721
MarleenEliza <62828450+MarleenEliza@users.noreply.github.com>
2022-03-27 13:19:21 +0900

    add alternative path for icon component

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -2,10 +2,10 @@ import classes from "components/IconButton.module.scss";
 
 function IconButton(props) {
   return (
-    <button>
+    <button onClick={props.onClick}>
       <img
         src={require(`assets/icons/${props.path}.svg`)}
-        alt="dynamic button"
+        alt={`${props.alt ?? props.path} icon`}
       />
       <span className={classes.tooltip}>{props.tooltip}</span>
     </button>
